6a6c41a5d67049b0aa799f8627d4d0ead859dd23
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-10-08 11:26:54 +0100

    removed unused initial files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles['App']}>
+      <h1>Wooshi Mint Page</h1>
+    </div>
+  );
+}
+
+export default App;
